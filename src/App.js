2ead712242b7d322891661650f7451608afbb3cb
dxwebster <adrianalimafm@gmail.com>
2020-09-23 09:30:03 -0300

    Add button

--- src/App.js
+++ src/App.js
@@ -1,87 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import './styles/App.css';
+import React, { Component } from 'react';
+import cx from 'classnames';
 
 
-import api from './services/api';
+export default class TodoList extends Component {
 
-function App() {
-
-  // Consumindo API Utilizando o Axios
-  const [repository, setRepository] = useState('');
-
-  useEffect(() => {
-
-    async function getContent() {
-      try {
-        const response = await api.get(`repos/facebook/react`);
-        setRepository(response.data) // pego as informações utilizando o data e coloca no respository
-
-        console.log(response.data)
-
-      } catch (error) {
-        console.error(error)
-      }
-    }
-    getContent();
-  }, [])
-
-
-  // Consumindo API Utilizando o Fetch
-  const [planetData, setPlanetData] = useState('');
-
-  useEffect(() => {
-
-    async function getContent() {
-      try {
-        const planetNumber = Math.floor(Math.random() * 61) + 1
-        const url = `https://swapi.dev/api/planets/${planetNumber}`
-
-        const response = await fetch(url)
-        const data = await response.json(); // precisa transformar as informações em Json e coloca no planetData
-
-        setPlanetData(data)
-
-      } catch (error) {
-        console.error(error)
-        const message = document.querySelector('main')
-        message.innerHTML = `<p class='error'>Desculpe, não foi possível carregar os dados.</p>`
-      }
+    state = {
+        todo: "",
+        todos : [],
+        active: false,
     }
 
-    getContent();
-  }, []) 
+    constructor(props) {
+        super(props);
 
-    return (
-      <div className="app">
+        this.todoChange = this.todoChange.bind(this);
+        this.todoClick = this.todoClick.bind(this);
+        this.todoComplete = this.todoComplete.bind(this);
 
-        <main>
-          <section>
-            <h1> Listando informações de um Repositório do Github</h1>
-            <p>Consumindo API do github utilizando o Axios</p>
+    }
 
-            <li>Nome do Repositório: {repository.full_name}</li>
-            <li>Descrição: {repository.description}</li>
-            <li>Login: {repository.login}</li>
-            <li>Open Issues: {repository.open_issues}</li>
-            <li>Size: {repository.owner.login}</li>
-          </section>
+    render() {
+              
+        return (
+            <>
+                <div>
+                    <h2>Todo List</h2>
 
-          
-          <section>
-            <h1> Lista de Planetas do Star Wars</h1>
-            <p>Consumindo API do github utilizando o Fetch</p>
+                     <input onChange={ this.todoChange } type="text" value={ this.state.todo }/>
+                     <button onClick={ this.todoClick }>Adicionar</button>
+                     <p><span>0</span> remaining out of <span>0</span> tasks</p>
+                      
+                    
+                    <ul>{ this.state.todos.map(todo => <li key={ todo } class={this.state.active ? 'is-done' : null} onClick={this.todoComplete}>{ todo }</li>) }</ul>
+                </div>
+                <style>{`.is-done {text-decoration: line-through;}`}</style>
+            </>
+        );
+    }
 
-            <li>Nome do Planeta: {planetData.name}</li>
-            <li>População: {planetData.population}</li>
-            <li>Clima: {planetData.climate}</li>
-            <li>Terreno: {planetData.terrain}</li>
-          </section>
+    todoChange(event) {
+        this.setState({ todo : event.target.value });
+    }
 
-        </main>
+    todoClick() {
+      if(!this.state.todo == " "){
+          this.setState({ todos : [].concat(this.state.todos, this.state.todo) });
+      }   
+    }
 
-      </div>
-    );
+    todoComplete(){
+       const currentState = this.state.active
+       this.setState({active: !currentState})
+    }
 
+    
 }
-
-export default App;
